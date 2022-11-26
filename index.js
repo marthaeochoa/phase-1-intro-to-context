a2abb1e7c2dfb20563e1ca5654df19f6faef3f89
martha ochoa <marthaeochoa98@gmail.com>
2022-11-25 18:36:26 -0600

    commit tree ac09272d23e7df3c298e9239044c6690d1711c8e

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
 // Your code here
+function createEmployeeRecord(employee){
+    
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees){
+  return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(eRecordObj, dateStamp){
+    let obj = {
+        type: "TimeIn", 
+        hour: parseInt(dateStamp.slice(-4)), 
+        date: dateStamp.slice(0, 10)
+    }
+    eRecordObj.timeInEvents.push(obj)
+   
+    return eRecordObj
+}
+
+function createTimeOutEvent(eRecordObj, dateStamp){
+    let obj = {
+        type: "TimeOut", 
+        hour: parseInt(dateStamp.slice(-4)), 
+        date: dateStamp.slice(0, 10)
+    }
+    eRecordObj.timeOutEvents.push(obj)
+
+    return eRecordObj   
+}
+
+function hoursWorkedOnDate(eRecordObj, date){
+    let hours;
+    
+    for (let i=0; i<eRecordObj.timeInEvents.length; i++){
+        if (eRecordObj.timeInEvents[i].date === date){
+            if (eRecordObj.timeOutEvents[i].date === date){
+                hours = eRecordObj.timeOutEvents[i].hour - eRecordObj.timeInEvents[i].hour
+            }
+        }
+    }
+
+    return hours/100
+}
+
+function wagesEarnedOnDate(eRecordObj, date){
+    return (hoursWorkedOnDate(eRecordObj, date)) * eRecordObj.payPerHour
+}
+
+function allWagesFor(eRecordObj){
+    let allPay = [];
+    let allDates = [];
+
+    for (let i = 0; i < eRecordObj.timeInEvents.length; i++){
+        allDates.push(eRecordObj.timeInEvents[i].date)
+    }
+
+    allDates.forEach(date => {
+        allPay.push(wagesEarnedOnDate(eRecordObj, date))
+    });
+
+    return allPay.reduce(( previousValue, currentValue ) => previousValue + currentValue)
+}
+
+function calculatePayroll(arrOfERecordObj){
+    let payroll = [];
+
+    arrOfERecordObj.forEach(employee => {
+        payroll.push(allWagesFor(employee)) 
+    });
+
+    return payroll.reduce((previousValue, currentValue) => previousValue + currentValue)
+}
+
